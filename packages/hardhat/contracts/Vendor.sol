d99e790759d15bda1b07f2e70f0deaf971cb8180
Andrzej <rumcaajss@gmail.com>
2022-01-25 11:38:31 +0100

    checkpoint 3

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,24 +1,35 @@
 pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
-//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
   YourToken yourToken;
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
 
   constructor(address tokenAddress) public {
     yourToken = YourToken(tokenAddress);
   }
 
-
   // ToDo: create a payable buyTokens() function:
-
+  function buyTokens() public payable {
+    uint256 tokensToTransfer = msg.value * tokensPerEth;
+    bool sent = yourToken.transfer(msg.sender, tokensToTransfer);
+    require(sent, "Something went wrong with transferring tokens");
+    emit BuyTokens(msg.sender, msg.value, tokensToTransfer);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
-
+  function withdraw() public onlyOwner {
+    (bool success, ) = msg.sender.call{value: address(this).balance}("");
+    require(success, "Something went wrong withdrawing ether");
+    
+  }
   // ToDo: create a sellTokens() function:
 
 
